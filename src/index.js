0badd7502158a09c68e39bd73ee5e9278746b79d
kajal24mapare <kajalmapare24@gmail.com>
2021-07-14 13:15:57 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+/*
+  Stacks
+  functions: push, pop, peek, length
+*/
+var letters = [];
+var word = "racecar";
+var rWord = "";
+for (var i = 0; i < word.length; i++) {
+  letters.push(word[i]);
+}
+for (var j = 0; j < word.length; j++) {
+  rWord += letters.pop();
+}
+if (word === rWord) {
+  console.log(word, "is Palindrome");
+} else {
+  console.log(word, "is not a Palindrome");
+}
 
-import App from "./App";
+// Create a stack
+var Stack = function () {
+  this.count = 0;
+  this.storage = {};
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+  this.push = function (value) {
+    this.storage[this.count] = value;
+    this.count++;
+  };
+
+  this.pop = function () {
+    if (this.count === 0) {
+      return undefined;
+    }
+    this.count--;
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  };
+
+  this.size = function () {
+    return this.count;
+  };
+
+  this.peek = function () {
+    return this.storage[this.count - 1];
+  };
+};
+
+var myStack = new Stack();
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
+myStack.push("kajal");
+console.log(myStack.size());
+//----------------------------------
