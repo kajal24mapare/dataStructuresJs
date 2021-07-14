2a1c7386ec23192bc8881eaefb8e399bfc94f88a
kajal24mapare <kajalmapare24@gmail.com>
2021-07-14 14:50:40 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -57,3 +57,108 @@ console.log(myStack.peek());
 myStack.push("kajal");
 console.log(myStack.size());
 //----------------------------------
+
+/*
+Sets
+functions: add, remove, has, values, size
+*/
+function mySet() {
+  var collection = [];
+
+  this.has = function (element) {
+    return collection.indexOf(element) !== -1;
+  };
+
+  this.values = function () {
+    return collection;
+  };
+
+  this.add = function (element) {
+    if (!this.has(element)) {
+      collection.push(element);
+      return true;
+    }
+    return false;
+  };
+
+  this.remove = function (element) {
+    if (this.has(element)) {
+      var index = collection.indexOf(element);
+      collection.splice(index, 1);
+      return true;
+    }
+    return false;
+  };
+
+  this.size = function () {
+    return collection.length;
+  };
+
+  this.union = function (otherSet) {
+    var unionSet = new mySet();
+    var firstSet = this.values();
+    var secondSet = otherSet.values();
+    firstSet.forEach(function (e) {
+      unionSet.add(e);
+    });
+    secondSet.forEach(function (e) {
+      secondSet.add(e);
+    });
+    return unionSet;
+  };
+
+  this.intersection = function (otherSet) {
+    var intersectionSet = new mySet();
+    var firstSet = this.values();
+    firstSet.forEach(function (e) {
+      if (otherSet.has(e)) {
+        intersectionSet.add(e);
+      }
+    });
+    return intersectionSet;
+  };
+
+  this.difference = function (otherSet) {
+    var differenceSet = new mySet();
+    var firstSet = this.values();
+    firstSet.forEach(function (e) {
+      if (!otherSet.has(e)) {
+        differenceSet.add(e);
+      }
+    });
+    return differenceSet;
+  };
+
+  // check if this set is subset of other set
+  this.subset = function (otherSet) {
+    var firstSet = this.values();
+    return firstSet.every(function (e) {
+      return otherSet.has(e);
+    });
+  };
+}
+
+var setA = new mySet();
+var setB = new mySet();
+setA.add("a");
+setB.add("a");
+setB.add("b");
+setB.add("c");
+setB.add("d");
+console.log(setA.subset(setB));
+console.log(setA.intersection(setB).values());
+console.log(setB.difference(setA).values());
+
+var setC = new Set();
+var setD = new Set();
+setC.add("a");
+setD.add("a");
+setD.add("b");
+setD.add("c");
+setD.add("d");
+console.log(setD.values());
+setD.delete("a");
+console.log(setD.has("a"));
+setD.add("d");
+
+//===================================
